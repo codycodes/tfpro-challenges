6cc87182699d04bb8bc59923df38c25bc193c2d8
Cody Gagnon <codycodes@users.noreply.github.com>
2025-05-05 18:45:16 -0700

    challenge-6 solution

--- challenge-6/challenge-6.tf
+++ challenge-6/challenge-6.tf
@@ -1,32 +1,63 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "5.80.0"
     }
   }
 }
 
 provider "aws" {
- region = "us-east-1"
+  region = "us-east-1"
+}
+
+provider "aws" {
+  region                   = "us-east-1"
+  alias                    = "iam-access"
+  profile                  = "iam-access"
+  shared_config_files      = ["./.aws/config"]
+  shared_credentials_files = ["./.aws/credentials"]
+}
+
+provider "aws" {
+  region                   = "us-east-1"
+  alias                    = "ec2-access"
+  profile                  = "ec2-access"
+  shared_config_files      = ["./.aws/config"]
+  shared_credentials_files = ["./.aws/credentials"]
+}
+provider "aws" {
+  region                   = "us-east-1"
+  alias                    = "readonly-access"
+  profile                  = "readonly-access"
+  shared_config_files      = ["./.aws/config"]
+  shared_credentials_files = ["./base-folder/default-creds.txt"]
 }
 
 resource "aws_security_group" "allow_tls" {
-  name        = "demo-firewall"
+  provider = aws.ec2-access
 
+  name = "demo-firewall"
 }
 
-data "aws_caller_identity" "current" {}
+data "aws_caller_identity" "current" {
+  provider = aws.readonly-access
+}
 
 output "account_id" {
   value = data.aws_caller_identity.current.account_id
 }
 
+resource "aws_iam_role_policy_attachments_exclusive" "cw_full_access" {
+  policy_arns = ["arn:aws:iam::aws:policy/CloudWatchFullAccess"]
+  role_name   = aws_iam_role.cw_full_access.name
+}
 
 resource "aws_iam_role" "cw_full_access" {
-  name = "CloudWatchFullAccess"
-  managed_policy_arns = ["arn:aws:iam::aws:policy/CloudWatchFullAccess"] 
+  provider = aws.iam-access
 
+  # managed_policy_arns = ["arn:aws:iam::aws:policy/CloudWatchFullAccess"] # deprecated
+  name = "CloudWatchFullAccess"
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [{
